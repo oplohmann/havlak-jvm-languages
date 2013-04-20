9abe5b5e69ff1bfa0dd8c02fab3f2e58b2a17e9c
Oliver Plohmann <oliver@objectscape.org>
2013-04-20 21:11:54 +0200

    initial

---
+++ kotlin-initial/src/BasicBlock.kt
@@ -0,0 +1,59 @@
+/**
+ * Created with IntelliJ IDEA. 
+ * User: Nutzer
+ * Date: 22.01.13
+ * Time: 21:36
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.ArrayList
+
+class BasicBlock(val name: Int)
+{
+    var inEdges = ArrayList<BasicBlock>()
+    var outEdges = ArrayList<BasicBlock>()
+
+    constructor {
+        BasicBlock.incrementBasicBlocks()
+    }
+
+    fun toString() = "BB#" + name
+    fun getNumPred(): Int = inEdges.size()
+    fun getNumSucc(): Int = outEdges.size()
+
+    fun addInEdge (bb: BasicBlock) {
+        inEdges.add(bb)
+    }
+    fun addOutEdge(bb: BasicBlock) {
+        outEdges.add(bb)
+    }
+
+    fun dump(): Unit {
+        var res = "BB#" + name + " "
+        if (inEdges.size() > 0)
+            res += "\tin : " + inEdges
+        if (outEdges.size() > 0)
+            res += "\tout: " + outEdges
+        // println(res)
+    }
+
+    class object
+    {
+        var numBasicBlocks: Int = 0
+
+        // method made to return a dummy value as a work around for
+        // http://youtrack.jetbrains.com/issue/KT-904
+        fun setMyNumBasicBlocks(n: Int) : Int {
+            numBasicBlocks = n
+            return numBasicBlocks
+        }
+
+        fun incrementBasicBlocks() : String {
+            setMyNumBasicBlocks(numBasicBlocks + 1)
+            return ""
+        }
+
+        // method renamed from getNumBasicBlocks to getMyNumBasicBlocks as a work
+        // around for http://youtrack.jetbrains.com/issue/KT-904
+        fun getMyNumBasicBlocks(): Int = numBasicBlocks
+    }
+}
---
+++ kotlin-initial/src/CFG.kt
@@ -0,0 +1,50 @@
+/**
+ * Created with IntelliJ IDEA. 
+ * User: Nutzer
+ * Date: 22.01.13
+ * Time: 21:36
+ * To change this template use File | Settings | File Templates.
+ */
+//
+// class CFG
+//
+// CFG maintains a list of nodes, plus a start node.
+// That's it.
+//
+import java.util.HashMap
+import java.util.ArrayList
+
+class CFG()
+{
+    var startNode: BasicBlock? = null
+    var basicBlockMap = HashMap<Int, BasicBlock>()
+    var edgeList = ArrayList<BasicBlockEdge>()
+
+    fun createNode(name: Int): BasicBlock
+    {
+        var node = basicBlockMap.get(name)
+        if(node == null) {
+            val tmp = BasicBlock(name)
+            basicBlockMap.put(name, tmp)
+            node = tmp
+        }
+
+        if (getNumNodes() == 1)
+            startNode = node
+
+        return node!!
+    }
+
+    fun dump() {
+        for (bb in basicBlockMap.values())
+            bb.dump()
+    }
+
+    fun addEdge(edge: BasicBlockEdge): Unit {
+        edgeList.add(edge)
+    }
+
+    fun getNumNodes(): Int = basicBlockMap.size()
+    fun getDst(edge: BasicBlockEdge): BasicBlock = edge.to
+    fun getSrc(edge: BasicBlockEdge): BasicBlock = edge.from
+}
